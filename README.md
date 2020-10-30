f6b482c3dac2351a48af1a198a174c5c7c12f4de
Tanvir <34381989+tshahria@users.noreply.github.com>
2020-10-29 20:29:57 -0400

    fix

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# cs1300_project2
-# cs1300-project3
+# studio js
--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <!-- DO NOT ADD/CHANGE ANYTHING IN THIS FILE -->
-<?php include_once("home.html"); ?>
+<?php include_once("index.html"); ?>
