596790871b89d81b3f42f82b9164b385f5c0c76c
Tanvir <34381989+tshahria@users.noreply.github.com>
2020-10-29 20:22:14 -0400

    fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=2WjrmVKM7H_otuadZxPdmSFMTonpyUX_xnsh5aDv81c";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -15,17 +15,34 @@ const doCORSRequest = (options) => {
 const corsPromise = () =>
   new Promise((resolve, reject) => {
     const request = doCORSRequest({
-      url: "https://trefle.io/api/v1/plants" + apiToken,
+      url: "https://trefle.io/api/v1/divisions" + apiToken,
     });
     resolve(request);
   });
 
 // THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
-corsPromise().then(
+let on_button_click = () => {
+  corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      console.log(JSON.parse(request.response)['data'])
+      let data = JSON.parse(request.response)['data']
+      .filter((x) => {
+        return x['name'].length > 11 ? true: false;
+      })
+      .map(x=>x['name']);
+      
+      document.getElementById("my_list").inner_html = "";
+      data.forEach((x) => {
+        var node = document.createElement("li");                 
+        var textnode = document.createTextNode(x);         
+        node.appendChild(textnode);                              
+        document.getElementById("my_list").appendChild(node); 
+      });
     })
 );
+};
 
+document.getElementById("load").addEventListener("click", on_button_click); 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
